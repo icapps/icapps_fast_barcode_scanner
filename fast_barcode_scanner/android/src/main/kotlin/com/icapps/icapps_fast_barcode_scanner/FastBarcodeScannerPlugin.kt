cfc959ce3b5772d7db61cd69ea26d60249001674
Jordy de Jonghe <jordy.dejonghe@icapps.com>
2025-11-06 14:53:59 +0100

    add import

--- fast_barcode_scanner/android/src/main/kotlin/com/icapps/icapps_fast_barcode_scanner/FastBarcodeScannerPlugin.kt
+++ fast_barcode_scanner/android/src/main/kotlin/com/icapps/icapps_fast_barcode_scanner/FastBarcodeScannerPlugin.kt
@@ -14,6 +14,7 @@ import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
+import io.flutter.plugin.common.MethodChannel.Result
 import io.flutter.plugin.common.PluginRegistry
 
 /** FastBarcodeScannerPlugin */
